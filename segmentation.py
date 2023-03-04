5588eb00584b423226a9e2bb502b1498eaf42935
andrew <asc269@cornell.edu>
2023-03-03 21:36:55 -0500

    Handle color images - delete grayscale(img)

--- segmentation.py
+++ segmentation.py
@@ -1,8 +1,18 @@
 import numpy as np
 
+def dist(v1, v2):
+  """
+  v1, v2: vectors, numpy arrays
+  Returns distance between v1 and v2
+  """
+  result = 0
+  for e1, e2 in zip(v1, v2):
+    result += (e1 - e2) ** 2
+  return np.sqrt(result)
+
 def weight(img, i1, j1, i2, j2):
   """
-  img: grayscale img? (how do to handle color image?)
+  img: color img
   (i1, j1): first coordinate
   (i2, j2): second coordinate
   Returns output of weight function specified in hw
@@ -11,15 +21,15 @@ def weight(img, i1, j1, i2, j2):
     return 0
   
   if abs(i1 - i2) <= 20 and abs(j1 - j2) <= 20:
-    c2 = img[i1][j1]
-    c1 = img[i2][j2] #how do we get color difference of color image?
-    return np.exp(-100 * abs(c1 - c2) ** 2)
+    c1 = img[i1][j1]
+    c2 = img[i2][j2]
+    return np.exp(-100 * dist(c1, c2) ** 2)
   
   return 0
 
 def create_W(img):
   """
-  img: grayscale img? (how do to handle color image?)
+  img: color img
   Returns W matrix as specified in hw
   """
   m, n, _ = img.shape
@@ -35,7 +45,7 @@ def create_W(img):
 
 def create_D(img):
   """
-  img: grayscale img? (how to handle color image?)
+  img: color img
   Returns D matrix as specified in hw
   """
   m, n, _ = img.shape
@@ -52,7 +62,7 @@ def create_D(img):
 
 def create_A(img):
   """
-  img: gray-scale img
+  img: color img
   Returns matrix A as specified in hw
   """
   m, n, _ = img.shape
@@ -68,19 +78,7 @@ def create_A(img):
     D_inverse = 1 / [D_inverse[i, j]] #inverse of a diagonal matrix is its reciprocal of its diagonals
   
   return I - np.matmul(D_inverse, W) #find A as specified in hw
-
-def grayscale(img):
-  """
-  img: color image
-  Returns grayscale equivalent of img
-  """
-  m, n, _ = img.shape
-  result = np.zeros((m, n))
-  for i in range(m):
-    for j in range(n):
-      r, g, b = img[i][j][0], img[i][j][0], img[i][j][0]
-      result[i, j] = 0.2125 * r + 0.7154 * g + 0.0721 * b
-  return result
+  
 
 def vectorize(img):
   """
